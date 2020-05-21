c54b10e9a7bc3cee57ad9a35f18d7f63f296bb54
dj505 <jaydenb505@gmail.com>
2020-05-21 00:54:05 -0600

    Changed filename again

--- BGAnimations/ScreenLogo overlay/default.lua
+++ BGAnimations/ScreenLogo overlay/default.lua
@@ -8,7 +8,7 @@ local t = Def.ActorFrame {
             end;
     };
 
-    LoadActor(THEME:GetPathG("","logoBlur"))..{
+    LoadActor(THEME:GetPathG("","blurLogo"))..{
         InitCommand=function(self)
             self:Center()
             :zoom(0.49,0.49)
