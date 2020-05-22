771c721823d20c93852eedf122e33f62749e6306
dj505 <jaydenb505@gmail.com>
2020-05-21 23:36:02 -0600

    More small changes

--- BGAnimations/ScreenSelectMusic overlay/CornerArrows.lua
+++ BGAnimations/ScreenSelectMusic overlay/CornerArrows.lua
@@ -3,44 +3,70 @@ local t = Def.ActorFrame {
     LoadActor(THEME:GetPathG("","ShiftUL"))..{
         InitCommand=function(self)
             self:zoom(0.10)
-            :horizalign(left)
-            :vertalign(top)
             :xy(SCREEN_LEFT+200,SCREEN_TOP+200)
             :decelerate(0.75)
-            :xy(SCREEN_LEFT+10,SCREEN_TOP+10)
+            :xy(SCREEN_LEFT+60,SCREEN_TOP+60)
         end;
     };
 
     LoadActor(THEME:GetPathG("","ShiftUR"))..{
         InitCommand=function(self)
             self:zoom(0.10)
-            :horizalign(right)
-            :vertalign(top)
             :xy(SCREEN_RIGHT-200,SCREEN_TOP+200)
             :decelerate(0.75)
-            :xy(SCREEN_RIGHT-10,SCREEN_TOP+10)
+            :xy(SCREEN_RIGHT-60,SCREEN_TOP+60)
         end;
     };
 
     LoadActor(THEME:GetPathG("","ShiftDL"))..{
         InitCommand=function(self)
             self:zoom(0.10)
-            :horizalign(left)
-            :vertalign(bottom)
             :xy(SCREEN_LEFT+200,SCREEN_BOTTOM-200)
             :decelerate(0.75)
-            :xy(SCREEN_LEFT+10,SCREEN_BOTTOM-10)
+            :xy(SCREEN_LEFT+60,SCREEN_BOTTOM-60)
         end;
     };
 
     LoadActor(THEME:GetPathG("","ShiftDR"))..{
         InitCommand=function(self)
             self:zoom(0.10)
-            :horizalign(right)
-            :vertalign(bottom)
             :xy(SCREEN_RIGHT-200,SCREEN_BOTTOM-200)
             :decelerate(0.75)
-            :xy(SCREEN_RIGHT-10,SCREEN_BOTTOM-10)
+            :xy(SCREEN_RIGHT-60,SCREEN_BOTTOM-60)
+        end;
+    };
+
+    --Glowy Arrows
+
+    LoadActor(THEME:GetPathG("","GlowShiftDL"))..{
+        InitCommand=function(self)
+            self:diffusealpha(0)
+            :zoom(0.10)
+            :xy(SCREEN_LEFT+60,SCREEN_BOTTOM-60)
+        end;
+        PreviousSongMessageCommand=function(self)
+        self:stoptweening()
+            :accelerate(0.2)
+            :diffusealpha(0.6)
+            :zoom(0.11)
+            :decelerate(0.2)
+            :diffusealpha(0)
+        end;
+    };
+
+    LoadActor(THEME:GetPathG("","GlowShiftDR"))..{
+        InitCommand=function(self)
+            self:diffusealpha(0)
+            :zoom(0.10)
+            :xy(SCREEN_RIGHT-60,SCREEN_BOTTOM-60)
+        end;
+        NextSongMessageCommand=function(self)
+            self:stoptweening()
+            :accelerate(0.2)
+            :diffusealpha(0.6)
+            :zoom(0.11)
+            :decelerate(0.2)
+            :diffusealpha(0)
         end;
     };
 };
