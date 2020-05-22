a8dbd663978d56f8b047534243c0789c3088622b
dj505 <jaydenb505@gmail.com>
2020-05-22 17:13:50 -0600

    Fixed 2 part select

--- Graphics/ChartInfo/default.lua
+++ Graphics/ChartInfo/default.lua
@@ -1,18 +1,22 @@
-local t = Def.ActorFrame {
+local t = Def.ActorFrame {};
 
-    LoadActor("ChartInfoP1")..{
-        Initcommand=function(self)
-            self:diffusealpha(0)
-            :xy(SCREEN_CENTER_X,SCREEN_CENTER_Y)
+for player in ivalues(GAMESTATE:GetEnabledPlayers()) do
+    t[#t+1] = LoadActor("ChartInfo"..pname(player))..{
+    InitCommand=function(self)
+        self:xy(SCREEN_CENTER_X,SCREEN_CENTER_Y-53)
+        :diffusealpha(0)
+        :zoom(0.2405)
+    end;
+    SongChosenMessageCommand=function(self)
+        self:diffusealpha(1);
+        if player == PLAYER_1 then
+            self:decelerate(0.25)
+            :x(SCREEN_CENTER_X-100);
+        elseif player == PLAYER_2 then
+            self:decelerate(0.25)
+            :x(SCREEN_CENTER_X+100);
         end;
     };
-
-    LoadActor("ChartInfoP2")..{
-        Initcommand=function(self)
-            self:diffusealpha(0)
-            :xy(SCREEN_CENTER_X,SCREEN_CENTER_Y)
-        end;
-    };
-};
+end;
 
 return t;
