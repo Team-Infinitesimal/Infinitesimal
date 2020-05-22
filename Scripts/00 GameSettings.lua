4035b1cd75eee4e8495087173dcec73b524f9ec1
SheepyChris <christopher.reinbrecht@gmail.com>
2020-05-22 16:58:43 -0300

    Add scorecap function for lifebar

--- Scripts/00 GameSettings.lua
+++ Scripts/00 GameSettings.lua
@@ -3,3 +3,9 @@ function SetPlayMode()
     GAMESTATE:SetCurrentStyle("Single");
     return "ScreenSelectMusic"
 end
+
+--Adds commas to your score, apparently
+function scorecap(n) -- credit http://richard.warburton.it
+	local left,num,right = string.match(n,'^([^%d]*%d)(%d*)(.-)$')
+	return left..(num:reverse():gsub('(%d%d%d)','%1.'):reverse())..right
+end
