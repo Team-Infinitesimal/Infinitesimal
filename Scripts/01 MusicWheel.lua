b334acc17a3693b7894dbcf536bf1a9109a9a56b
dj505 <jaydenb505@gmail.com>
2020-05-22 18:57:52 -0600

    Back arrows only show during 2 part select

--- Scripts/01 MusicWheel.lua
+++ Scripts/01 MusicWheel.lua
@@ -1,6 +1,6 @@
 function wheelTransformNyoom(self,offsetFromCenter,itemIndex,numItems)
-    self:x(offsetFromCenter*380);
+    self:x(offsetFromCenter*385);
     local rot = -20;
-    self:rotationy(offsetFromCenter*-5);
+    self:rotationy(offsetFromCenter*5);
     self:z(math.abs(offsetFromCenter*5)*-10);
 end;
