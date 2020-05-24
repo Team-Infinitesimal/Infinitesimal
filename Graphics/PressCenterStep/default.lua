93ed3676724eb63f8ab2928a20e26b816cdceef3
dj505 <jaydenb505@gmail.com>
2020-05-23 21:32:29 -0600

    ScreenTitleMenu improvements

--- Graphics/PressCenterStep/default.lua
+++ Graphics/PressCenterStep/default.lua
@@ -3,18 +3,37 @@ local t = Def.ActorFrame{
     LoadActor("centerstep")..{
         InitCommand=function(self)
             self:y(55)
-            :zoom(0.2,0.2)
+            :zoom(0.08,0.08)
+            end;
+    };
+
+    LoadActor("centerstep")..{
+        InitCommand=function(self)
+            self:y(55)
+            :zoom(0.08,0.08)
+            :diffusealpha(0)
+            :queuecommand("FadeEffect")
+            end;
+        FadeEffectCommand=function(self)
+            self:stoptweening()
+            :zoom(0.08,0.08)
+            :diffusealpha(0.75)
+            :decelerate(0.3947)
+            :zoom(0.1,0.1)
+            :diffusealpha(0)
+            :queuecommand("FadeEffect")
             end;
     };
 
     LoadActor("press")..{
         InitCommand=function(self)
-            self:zoom(0.2,0.2)
+            self:zoom(0.08,0.08)
             end;
         OnCommand=function(self)
             self:bounce()
+            :sleep(1.1)
             :effectmagnitude(0,-18,0)
-            :effectperiod(0.42)
+            :effectperiod(0.3947)
             end;
     };
 };
