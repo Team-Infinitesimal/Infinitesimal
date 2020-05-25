e7883d5aa27fdf4af28980f35a8b04ef84e16911
dj505 <jaydenb505@gmail.com>
2020-05-24 21:15:58 -0600

    Began work on ScreenEvaluation

--- BGAnimations/ScreenEvaluation overlay/default.lua
+++ BGAnimations/ScreenEvaluation overlay/default.lua
@@ -1,5 +1,35 @@
 local t = Def.ActorFrame {
 
+    LoadActor(THEME:GetPathG("","EvalElements/GridLines"))..{
+        InitCommand=function(self)
+            self:diffusealpha(0)
+            :zoomx(0)
+            :zoomy(0.1)
+            :xy(SCREEN_CENTER_X,SCREEN_CENTER_Y)
+            end;
+        OnCommand=function(self)
+            self:sleep(1.25)
+            :diffusealpha(0.25)
+            :decelerate(0.5)
+            :zoomx(0.15)
+            end;
+    };
+
+    LoadActor(THEME:GetPathG("","EvalElements/CenterColumn"))..{
+        InitCommand=function(self)
+            self:diffusealpha(0)
+            :zoomy(0)
+            :zoomx(0.1)
+            :xy(SCREEN_CENTER_X,SCREEN_CENTER_Y)
+            end;
+        OnCommand=function(self)
+            self:sleep(1)
+            :diffusealpha(1)
+            :decelerate(0.3)
+            :zoomy(0.1)
+            end;
+    };
+
     LoadActor(THEME:GetPathG("","ScreenHudTop"))..{
         InitCommand=function(self)
             self:diffusealpha(0)
