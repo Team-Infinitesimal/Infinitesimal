96c6013ac6355e491e6f4b7d9f9bdf797a3920e9
SheepyChris <christopher.reinbrecht@gmail.com>
2020-05-31 16:31:21 -0300

    Adjust the song wheel to display less but larger banners

--- Scripts/02 MusicWheel.lua
+++ Scripts/02 MusicWheel.lua
@@ -1,7 +1,6 @@
 function wheelTransformNyoom(self,offsetFromCenter,itemIndex,numItems)
-    self:zoom(0.85)
-    self:x(offsetFromCenter*270);
+    self:x(offsetFromCenter*350);
     local rot = -20;
-    self:rotationy(offsetFromCenter*15);
+    self:rotationy(offsetFromCenter*5);
     self:z(math.abs(offsetFromCenter*5)*-10);
 end;
