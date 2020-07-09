252b81865bf45178398d4df524ee9f8e37d669b0
dj505 <jaydenb505@gmail.com>
2020-07-08 23:27:37 -0600

    Changed dots to commas in score

--- Scripts/00 GameSettings.lua
+++ Scripts/00 GameSettings.lua
@@ -6,8 +6,8 @@ end;
 
 --Adds commas to your score, apparently
 function scorecap(n) -- credit http://richard.warburton.it
-	local left,num,right = string.match(n,'^([^%d]*%d)(%d*)(.-)$')
-	return left..(num:reverse():gsub('(%d%d%d)','%1.'):reverse())..right
+	local left,num,right = string.match(n,'^([^%d]*%d)(%d*)(,-)$')
+	return left..(num:reverse():gsub('(%d%d%d)','%1,'):reverse())..right
 end;
 
 function GetChartType(player)
