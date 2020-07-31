b188e0a0f3b1b564870d284197414d1c42fb8d00
SheepyChris <christopher.reinbrecht@gmail.com>
2020-07-31 02:40:07 -0400

    Fix ScreenTitleMenu for wider aspect ratios

--- BGAnimations/ScreenTitleMenu overlay/default.lua
+++ BGAnimations/ScreenTitleMenu overlay/default.lua
@@ -4,7 +4,11 @@ local t = Def.ActorFrame{
         InitCommand=function(self)
             self:xy(SCREEN_CENTER_X, SCREEN_CENTER_Y-30)
             :zoom(0.5,0.5)
-            end;
+			
+			if GetScreenAspectRatio() >= 1.5 then
+				self:y(SCREEN_CENTER_Y)
+			end;
+        end;
     };
 
     LoadActor(THEME:GetPathG("","blurLogo"))..{
@@ -13,7 +17,11 @@ local t = Def.ActorFrame{
             :zoom(0.49,0.49)
             :diffusealpha(0)
             :queuecommand("Flash")
-            end;
+			
+			if GetScreenAspectRatio() >= 1.5 then
+				self:y(SCREEN_CENTER_Y)
+			end;
+        end;
         FlashCommand=function(self)
             self:sleep(3)
             :decelerate(1)
@@ -22,21 +30,29 @@ local t = Def.ActorFrame{
             :accelerate(1)
             :diffusealpha(0)
             :queuecommand("Flash")
-            end;
+        end;
     };
 
     LoadActor(THEME:GetPathG("","PressCenterStep"))..{
         InitCommand=function(self)
             self:xy(SCREEN_LEFT+107,350)
             :zoom(0.75,0.75)
-            end;
+			
+			if GetScreenAspectRatio() >= 1.5 then
+				self:y(300)
+			end;
+        end;
     };
 
     LoadActor(THEME:GetPathG("","PressCenterStep"))..{
         InitCommand=function(self)
             self:xy(SCREEN_RIGHT-107,350)
             :zoom(0.75,0.75)
-            end;
+			
+			if GetScreenAspectRatio() >= 1.5 then
+				self:y(300)
+			end;
+        end;
     };
 };
 
