a45f46247ed6504e3bb25ffe4076bb0162e8eae2
SheepyChris <12992355+SheepyChris@users.noreply.github.com>
2020-09-23 16:02:12 -0400

    Fix missing folder icons and other adjustments

--- BGAnimations/ScreenSelectPlayMode background.lua
+++ BGAnimations/ScreenSelectPlayMode background.lua
@@ -7,6 +7,32 @@ local t = Def.ActorFrame {
             :diffusealpha(0.8)
             end;
     };
+    
+    LoadActor(THEME:GetPathG("","ScreenHudTop"))..{
+        InitCommand=function(self)
+            self:diffusealpha(0)
+            :vertalign(top)
+			:zoom(0.835)
+            :xy(SCREEN_CENTER_X,SCREEN_TOP-100)
+            :diffusealpha(1)
+            :sleep(0.25)
+            :decelerate(0.75)
+            :y(SCREEN_TOP)
+        end;
+    };
+
+    LoadActor(THEME:GetPathG("","ScreenHudBottom"))..{
+        InitCommand=function(self)
+            self:diffusealpha(0)
+            :vertalign(bottom)
+			:zoom(0.835)
+            :xy(SCREEN_CENTER_X,SCREEN_BOTTOM+100)
+            :diffusealpha(1)
+            :sleep(0.25)
+            :decelerate(0.75)
+            :y(SCREEN_BOTTOM)
+        end;
+    };
 };
 
 return t;
--- BGAnimations/ScreenSelectPlayMode decorations/default.lua
+++ BGAnimations/ScreenSelectPlayMode decorations/default.lua
@@ -17,32 +17,6 @@ local t = Def.ActorFrame {
 		PREFSMAN:SetPreference("TimingWindowSecondsW5",0.187500);
     end;
 
-    LoadActor(THEME:GetPathG("","ScreenHudTop"))..{
-        InitCommand=function(self)
-            self:diffusealpha(0)
-            :vertalign(top)
-			:scaletocover(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT/8)
-            :xy(SCREEN_CENTER_X,SCREEN_TOP-100)
-            :diffusealpha(1)
-            :sleep(0.25)
-            :decelerate(0.75)
-            :y(SCREEN_TOP)
-        end;
-    };
-
-    LoadActor(THEME:GetPathG("","ScreenHudBottom"))..{
-        InitCommand=function(self)
-            self:diffusealpha(0)
-            :vertalign(bottom)
-			:scaletocover(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT/8)
-            :xy(SCREEN_CENTER_X,SCREEN_BOTTOM+100)
-            :diffusealpha(1)
-            :sleep(0.25)
-            :decelerate(0.75)
-            :y(SCREEN_BOTTOM)
-        end;
-    };
-
     LoadActor("CornerArrows");
 
     LoadActor(THEME:GetPathG("","ModeSelect/ArcadeMode"))..{
