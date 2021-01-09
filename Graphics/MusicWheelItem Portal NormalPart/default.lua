fcc857ae99c363de6de49c6ec1f849fa9ef62e07
SheepyChris <12992355+SheepyChris@users.noreply.github.com>
2021-01-08 20:06:59 -0500

    Add graphic combo label

--- Graphics/MusicWheelItem Portal NormalPart/default.lua
+++ Graphics/MusicWheelItem Portal NormalPart/default.lua
@@ -1,21 +1,6 @@
-local bannerFirst = true;
-if GetUserPref("UserPrefWheelPriority") == "Banner" then
-	--Don't need to assign, since it's already true
-	--bannerFirst = true;
-elseif GetUserPref("UserPrefWheelPriority") == "Background" then
-	bannerFirst = false;
-else --Auto
-	if GAMESTATE:GetCurrentGame():GetName() == "pump" then
-		bannerFirst = true;
-	else
-		bannerFirst = false; --Prioritize jackets for every other game mode
-	end;
-end;
-
---local total = 0
 return Def.ActorFrame {
-	OnCommand=cmd(diffusealpha,1;zoom,1);
-	PlayerJoinedMessageCommand=cmd(playcommand,"On");
+	OnCommand=function(self)self:diffusealpha(1):zoom(1)end;
+	PlayerJoinedMessageCommand=function(self)self:playcommand("On")end;
 
 	-- banners
 	Def.Banner {
@@ -23,24 +8,8 @@ return Def.ActorFrame {
 		InitCommand=cmd(diffusealpha,1);
 		SetMessageCommand=function(self,params)
 			self:stoptweening();
-			local song = params.Song;
-			local path;
-			if song then
-				if bannerFirst then
-					path = song:GetBannerPath();
-					if not path then
-						path = song:GetBackgroundPath();
-					end;
-				else
-					path = song:GetBackgroundPath();
-					if not path then
-						path = song:GetBannerPath();
-					end;
-				end;
-
-			end;
 			if not path then path = THEME:GetPathG("Common","fallback songbanner") end
-			self:LoadFromCachedBanner(path);
+			self:LoadFromCachedBanner(THEME:GetPathG("","RandomBanner"));
 			self:scaletoclipped(300,168);
 		end;
 	};
