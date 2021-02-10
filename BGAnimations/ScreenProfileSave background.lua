3daa146af94c80b8aae4bb24ae8b52ae94d69798
dj505 <jaydenb505@gmail.com>
2021-02-09 22:31:42 -0700

    Small update to saving profile screen (it's good enough tbh)

--- BGAnimations/ScreenProfileSave background.lua
+++ BGAnimations/ScreenProfileSave background.lua
@@ -1,11 +1,17 @@
 local t = Def.ActorFrame {
 
-	Def.Quad {
+		LoadActor(THEME:GetPathG("", "GenericGradientBg"))..{
+				InitCommand=function(self)
+						self:scaletocover(0, 0, SCREEN_RIGHT, SCREEN_BOTTOM)
+				end;
+		};
+
+		Def.Quad {
         InitCommand=function(self)
             self:xy(SCREEN_CENTER_X,SCREEN_CENTER_Y)
-			:zoomx(SCREEN_WIDTH)
-			:zoomy(SCREEN_HEIGHT)
-            :diffuse(1,1,1,1)
+						:zoomx(SCREEN_WIDTH)
+						:zoomy(SCREEN_HEIGHT)
+            :diffuse(0,0,0,0.50)
         end;
     };
 
