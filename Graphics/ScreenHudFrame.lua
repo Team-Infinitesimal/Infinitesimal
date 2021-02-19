b2df2e60ef61885f7f29db7fe29b29400a680d5e
dj505 <jaydenb505@gmail.com>
2021-02-19 16:52:53 -0700

    Basic profile avatar support (finally!)

--- Graphics/ScreenHudFrame.lua
+++ Graphics/ScreenHudFrame.lua
@@ -25,8 +25,89 @@ local t = Def.ActorFrame {
             :y(SCREEN_BOTTOM)
         end;
     };
-    
+
 };
+
+-- Avatars
+for pn in ivalues(GAMESTATE:GetHumanPlayers()) do
+
+  t[#t+1] = LoadActor(THEME:GetPathG("", "EmptyAvatarSlot"))..{
+    InitCommand=function(self)
+      self:zoom(0.26)
+      if pn == "PlayerNumber_P1" then
+        self:x(SCREEN_CENTER_X - 138)
+        :y(SCREEN_BOTTOM + 80)
+      elseif pn == "PlayerNumber_P2" then
+        self:x(SCREEN_CENTER_X + 138)
+        :y(SCREEN_BOTTOM + 80)
+        :rotationy(180)
+      end;
+    end;
+
+    PlayerJoinedMessageCommand=function(self)self:queuecommand("On")end;
+		PlayerUnjoinedMessageCommand=function(self)self:queuecommand("On")end;
+		ReloadedProfilesMessageCommand=function(self)self:queuecommand("On")end;
+
+    OnCommand=function(self)
+			self:sleep(0.25)
+      :decelerate(0.75)
+			:diffusealpha(1)
+			:y(SCREEN_BOTTOM-28)
+		end;
+  }
+
+  t[#t+1] = LoadActor(THEME:GetPathG("", "AvatarMask"))..{
+    InitCommand=function(self)
+      self:zoom(0.26)
+      self:MaskSource()
+      if pn == "PlayerNumber_P1" then
+        self:x(SCREEN_CENTER_X - 138)
+        :y(SCREEN_BOTTOM + 80)
+      elseif pn == "PlayerNumber_P2" then
+        self:x(SCREEN_CENTER_X + 138)
+        :y(SCREEN_BOTTOM + 80)
+        :rotationy(180)
+      end;
+    end;
+
+    PlayerJoinedMessageCommand=function(self)self:queuecommand("On")end;
+		PlayerUnjoinedMessageCommand=function(self)self:queuecommand("On")end;
+		ReloadedProfilesMessageCommand=function(self)self:queuecommand("On")end;
+
+    OnCommand=function(self)
+			self:sleep(0.25)
+      :decelerate(0.75)
+			:diffusealpha(1)
+			:y(SCREEN_BOTTOM-28)
+		end;
+  }
+
+  t[#t+1] = Def.Sprite {
+    Texture = GetPlayerAvatar(pn);
+    InitCommand = function(self)
+      if pn == "PlayerNumber_P1" then
+        self:x(SCREEN_CENTER_X - 138)
+      elseif pn == "PlayerNumber_P2" then
+        self:x(SCREEN_CENTER_X + 138)
+      end;
+      self:y(SCREEN_BOTTOM + 80)
+      :zoomto(130,130)
+      :MaskDest();
+    end;
+
+    PlayerJoinedMessageCommand=function(self)self:queuecommand("On")end;
+		PlayerUnjoinedMessageCommand=function(self)self:queuecommand("On")end;
+		ReloadedProfilesMessageCommand=function(self)self:queuecommand("On")end;
+
+    OnCommand=function(self)
+			self:sleep(0.25)
+      :decelerate(0.75)
+			:diffusealpha(1)
+			:y(SCREEN_BOTTOM-28)
+		end;
+  };
+
+end;
 
 -- Profile name display
 for pn in ivalues(PlayerNumber) do
@@ -34,17 +115,17 @@ for pn in ivalues(PlayerNumber) do
 	t[#t+1] = LoadFont("Montserrat normal 40px")..{
 
 		InitCommand=function(self)
-			self:y(SCREEN_BOTTOM+80)
-			:zoom(0.35)
+			self:y(SCREEN_BOTTOM+85)
+			:zoom(0.3)
 			:diffuse(color("1,1,1,0"))
 			:shadowlength(1)
 
 			if (pn == PLAYER_1) then
 				self:horizalign(right)
-				self:x(SCREEN_CENTER_X-110)
+				self:x(SCREEN_CENTER_X-75)
 			else
 				self:horizalign(left)
-				self:x(SCREEN_CENTER_X+110)
+				self:x(SCREEN_CENTER_X+75)
 			end;
 
 			self:queuecommand("On");
@@ -67,12 +148,12 @@ for pn in ivalues(PlayerNumber) do
 			end;
 
 			self:sleep(0.25)
-            :decelerate(0.75)
+      :decelerate(0.75)
 			:diffusealpha(1)
-			:y(SCREEN_BOTTOM-20)
+			:y(SCREEN_BOTTOM-15)
 		end;
 	};
-	
+
 end;
 
 return t;
