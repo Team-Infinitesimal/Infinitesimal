db7849e189e2a371ab694973273297a08e9fc9a6
Jose_Varela <jo.lu6853@gmail.com>
2021-02-19 21:20:56 -0600

    Add FadeQuad
    A module that makes the process of fade in/out way easier

--- BGAnimations/fadeout_black.lua
+++ BGAnimations/fadeout_black.lua
@@ -1,18 +1,1 @@
-local t = Def.ActorFrame{
-
-    Def.Quad {
-        InitCommand=function(self)
-            self:Center()
-            :zoomto(SCREEN_WIDTH+10,SCREEN_HEIGHT+10)
-            end;
-
-        OnCommand=function(self)
-            self:diffuse(0,0,0,0)
-            :accelerate(0.5)
-            :diffusealpha(1)
-            :sleep(0.5)
-            end;
-    };
-};
-
-return t;
+return LoadModule("Actor.FadeQuad.lua")( 0.5, "accelerate", color("0,0,0,0"), Color.Black )
