bcd55f192ed4cd557f25bb11c05b3131694ad745
SheepyChris <12992355+SheepyChris@users.noreply.github.com>
2021-02-20 22:13:03 -0500

    Refactor profile icon display

--- Graphics/ScreenHudFrame.lua
+++ Graphics/ScreenHudFrame.lua
@@ -1,159 +1,139 @@
 local t = Def.ActorFrame {
 
-    LoadActor(THEME:GetPathG("","ScreenHudTop"))..{
+    Def.Sprite {
+		Texture="ScreenHudTop",
         InitCommand=function(self)
-            self:diffusealpha(0)
-            :vertalign(top)
+            self:vertalign(top)
 			:zoom(0.835)
             :xy(SCREEN_CENTER_X,SCREEN_TOP-100)
-            :diffusealpha(1)
-            :sleep(0.25)
+        end,
+        OnCommand=function(self)
+			self:sleep(0.25)
             :decelerate(0.75)
             :y(SCREEN_TOP)
-        end;
-    };
+        end
+    },
 
-    LoadActor(THEME:GetPathG("","ScreenHudBottom"))..{
+    Def.Sprite {
+		Texture="ScreenHudBottom",
         InitCommand=function(self)
-            self:diffusealpha(0)
-            :vertalign(bottom)
+            self:vertalign(bottom)
 			:zoom(0.835)
             :xy(SCREEN_CENTER_X,SCREEN_BOTTOM+100)
-            :diffusealpha(1)
-            :sleep(0.25)
+        end,
+        OnCommand=function(self)
+			self:sleep(0.25)
             :decelerate(0.75)
             :y(SCREEN_BOTTOM)
-        end;
-    };
+        end
+    }
 
-};
+}
 
 -- Avatars
-for pn in ivalues(GAMESTATE:GetHumanPlayers()) do
-
-  t[#t+1] = LoadActor(THEME:GetPathG("", "EmptyAvatarSlot"))..{
-    InitCommand=function(self)
-      self:zoom(0.26)
-      if pn == "PlayerNumber_P1" then
-        self:x(SCREEN_CENTER_X - 138)
-        :y(SCREEN_BOTTOM + 80)
-      elseif pn == "PlayerNumber_P2" then
-        self:x(SCREEN_CENTER_X + 138)
-        :y(SCREEN_BOTTOM + 80)
-        :rotationy(180)
-      end;
-    end;
-
-    PlayerJoinedMessageCommand=function(self)self:queuecommand("On")end;
-		PlayerUnjoinedMessageCommand=function(self)self:queuecommand("On")end;
-		ReloadedProfilesMessageCommand=function(self)self:queuecommand("On")end;
-
-    OnCommand=function(self)
-			self:sleep(0.25)
-      :decelerate(0.75)
-			:diffusealpha(1)
-			:y(SCREEN_BOTTOM-28)
-		end;
-  }
-
-  t[#t+1] = LoadActor(THEME:GetPathG("", "AvatarMask"))..{
-    InitCommand=function(self)
-      self:zoom(0.26)
-      self:MaskSource()
-      if pn == "PlayerNumber_P1" then
-        self:x(SCREEN_CENTER_X - 138)
-        :y(SCREEN_BOTTOM + 80)
-      elseif pn == "PlayerNumber_P2" then
-        self:x(SCREEN_CENTER_X + 138)
-        :y(SCREEN_BOTTOM + 80)
-        :rotationy(180)
-      end;
-    end;
-
-    PlayerJoinedMessageCommand=function(self)self:queuecommand("On")end;
-		PlayerUnjoinedMessageCommand=function(self)self:queuecommand("On")end;
-		ReloadedProfilesMessageCommand=function(self)self:queuecommand("On")end;
-
-    OnCommand=function(self)
-			self:sleep(0.25)
-      :decelerate(0.75)
-			:diffusealpha(1)
-			:y(SCREEN_BOTTOM-28)
-		end;
-  }
-
-  t[#t+1] = Def.Sprite {
-    Texture = GetPlayerAvatar(pn);
-    InitCommand = function(self)
-      if pn == "PlayerNumber_P1" then
-        self:x(SCREEN_CENTER_X - 138)
-      elseif pn == "PlayerNumber_P2" then
-        self:x(SCREEN_CENTER_X + 138)
-      end;
-      self:y(SCREEN_BOTTOM + 80)
-      :zoomto(130,130)
-      :MaskDest();
-    end;
-
-    PlayerJoinedMessageCommand=function(self)self:queuecommand("On")end;
-		PlayerUnjoinedMessageCommand=function(self)self:queuecommand("On")end;
-		ReloadedProfilesMessageCommand=function(self)self:queuecommand("On")end;
-
-    OnCommand=function(self)
-			self:sleep(0.25)
-      :decelerate(0.75)
-			:diffusealpha(1)
-			:y(SCREEN_BOTTOM-28)
-		end;
-  };
-
-end;
-
--- Profile name display
 for pn in ivalues(PlayerNumber) do
+	
+	t[#t+1] = Def.ActorFrame {
+	
+		Def.Sprite {
+			Texture="EmptyAvatarSlot",
+			InitCommand=function(self)
+				self:zoom(0.26)
+				:x(pn == PLAYER_1 and SCREEN_CENTER_X-138 or SCREEN_CENTER_X+138)
+				:y(SCREEN_BOTTOM + 80)
+				:rotationy(pn == PLAYER_1 and 0 or 180)
+				:queuecommand("On")
+				:sleep(0.25)
+				:decelerate(0.75)
+				:y(SCREEN_BOTTOM-28)
+			end,
 
-	t[#t+1] = LoadFont("Montserrat normal 40px")..{
+			PlayerJoinedMessageCommand=function(self)self:queuecommand("On")end,
+			PlayerUnjoinedMessageCommand=function(self)self:queuecommand("On")end,
+			ReloadedProfilesMessageCommand=function(self)self:queuecommand("On")end,
 
-		InitCommand=function(self)
-			self:y(SCREEN_BOTTOM+85)
-			:zoom(0.3)
-			:diffuse(color("1,1,1,0"))
-			:shadowlength(1)
+			OnCommand=function(self)
+				self:visible(GAMESTATE:IsHumanPlayer(pn))
+			end
+		},
 
-			if (pn == PLAYER_1) then
-				self:horizalign(right)
-				self:x(SCREEN_CENTER_X-75)
-			else
-				self:horizalign(left)
-				self:x(SCREEN_CENTER_X+75)
-			end;
+		Def.Sprite {
+			Texture="AvatarMask",
+			InitCommand=function(self)
+				self:zoom(0.26)
+				:x(pn == PLAYER_1 and (SCREEN_CENTER_X-138) or (SCREEN_CENTER_X+138))
+				:y(SCREEN_BOTTOM + 80)
+				:rotationy(pn == PLAYER_1 and 0 or 180)
+				:MaskSource()
+				:queuecommand("On")
+				:sleep(0.25)
+				:decelerate(0.75)
+				:y(SCREEN_BOTTOM-28)
+			end,
 
-			self:queuecommand("On");
-		end;
+			PlayerJoinedMessageCommand=function(self)self:queuecommand("On")end,
+			PlayerUnjoinedMessageCommand=function(self)self:queuecommand("On")end,
+			ReloadedProfilesMessageCommand=function(self)self:queuecommand("On")end,
 
-		PlayerJoinedMessageCommand=function(self)self:queuecommand("On")end;
-		PlayerUnjoinedMessageCommand=function(self)self:queuecommand("On")end;
-		ReloadedProfilesMessageCommand=function(self)self:queuecommand("On")end;
+			OnCommand=function(self)
+				self:visible(GAMESTATE:IsHumanPlayer(pn))
+			end
+		},
 
-		-- Update when a player joins
-		OnCommand=function(self)
-			if GAMESTATE:IsHumanPlayer(pn) then
-				GAMESTATE:LoadProfiles();
-				local profile = PROFILEMAN:GetProfile(pn):GetDisplayName();
-				if profile == "" then
-					self:settext("")
-				else
-					self:settext(profile)
-				end;
-			end;
+		Def.Sprite {
+			Texture=GetPlayerAvatar(pn),
+			InitCommand=function(self)
+				self:scaletocover(0,0,130,0)
+				:x(pn == PLAYER_1 and (SCREEN_CENTER_X-138) or (SCREEN_CENTER_X+138))
+				:y(SCREEN_BOTTOM + 80)
+				:MaskDest()
+				:queuecommand("On")
+				:sleep(0.25)
+				:decelerate(0.75)
+				:y(SCREEN_BOTTOM-28)
+			end,
 
-			self:sleep(0.25)
-      :decelerate(0.75)
-			:diffusealpha(1)
-			:y(SCREEN_BOTTOM-15)
-		end;
-	};
+			PlayerJoinedMessageCommand=function(self)self:queuecommand("On")end,
+			PlayerUnjoinedMessageCommand=function(self)self:queuecommand("On")end,
+			ReloadedProfilesMessageCommand=function(self)self:queuecommand("On")end,
 
-end;
+			OnCommand=function(self)
+				self:Load(GetPlayerAvatar(pn))
+				:visible(GAMESTATE:IsHumanPlayer(pn))
+			end
+		},
 
-return t;
+		LoadFont("Montserrat normal 40px")..{
+			InitCommand=function(self)
+				self:y(SCREEN_BOTTOM+80)
+				:zoom(0.3)
+				:diffuse(color("1,1,1,1"))
+				:shadowlength(1)
+				:horizalign(pn == PLAYER_1 and right or left)
+				:x(pn == PLAYER_1 and SCREEN_CENTER_X-80 or SCREEN_CENTER_X+80)
+				:queuecommand("On")
+				:sleep(0.25)
+				:decelerate(0.75)
+				:y(SCREEN_BOTTOM-16)
+			end,
+
+			PlayerJoinedMessageCommand=function(self)self:queuecommand("On")end,
+			PlayerUnjoinedMessageCommand=function(self)self:queuecommand("On")end,
+			ReloadedProfilesMessageCommand=function(self)self:queuecommand("On")end,
+
+			-- Update when a player joins
+			OnCommand=function(self)
+				if GAMESTATE:IsHumanPlayer(pn) then
+					GAMESTATE:LoadProfiles()
+					self:settext(PROFILEMAN:GetProfile(pn):GetDisplayName())
+				end
+
+				self:visible(GAMESTATE:IsHumanPlayer(pn))
+			end
+		}
+	
+	}
+
+end
+
+return t
