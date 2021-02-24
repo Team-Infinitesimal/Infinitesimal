826c6ccbddd85dc9526e710a032e790f28871cb5
dj505 <jaydenb505@gmail.com>
2021-02-24 02:12:03 -0700

    ✧･ﾟ: *✧ Fancy title screen animation *:･ﾟ✧*

--- BGAnimations/ScreenTitleMenu overlay.lua
+++ BGAnimations/ScreenTitleMenu overlay.lua
@@ -13,59 +13,93 @@ local SongStats = ("%i songs in %i groups"):format(
 
 local t = Def.ActorFrame {
 
-    Def.Sprite {
+	Def.ActorFrame {
+
+		InitCommand=function(self)
+			self:Center()
+			:queuecommand("Zoomy")
+		end,
+
+		ZoomyCommand=function(self)
+			self:accelerate(3.4288)
+			:zoom(0.96)
+			:decelerate(3.4288)
+			:zoom(1)
+			:queuecommand("Zoomy")
+		end,
+
+		Def.Sprite {
 		Texture=THEME:GetPathG("","logo"),
-        InitCommand=function(self)
-            self:Center()
-            :zoom(0.5,0.5)
-        end
+      InitCommand=function(self)
+        self:zoom(0.5,0.5)
+      end
+    },
+
+		Def.Sprite {
+		Texture=THEME:GetPathG("","logo"),
+      InitCommand=function(self)
+        self:zoom(0.5,0.5)
+				:queuecommand("Pulse")
+      end,
+
+			PulseCommand=function(self)
+				self:sleep(3.4288)
+				:diffusealpha(0.5)
+				:zoom(0.5)
+				:decelerate(1.7144)
+				:zoom(0.65)
+				:diffusealpha(0)
+				:sleep(1.7144)
+				:queuecommand("Pulse")
+			end,
     },
 
     Def.Sprite {
 		Texture=THEME:GetPathG("","blurLogo"),
-        InitCommand=function(self)
-            self:Center()
-            :zoom(0.49,0.49)
-            :diffusealpha(0)
-            :queuecommand("Flash")
-        end,
-        FlashCommand=function(self)
-            self:accelerate(3.4288)
-            :diffusealpha(1)
-            :decelerate(3.4288)
-            :diffusealpha(0)
-            :queuecommand("Flash")
-        end
+      InitCommand=function(self)
+        self:zoom(0.8,0.8)
+        :diffusealpha(0)
+        :queuecommand("Flash")
+      end,
+      FlashCommand=function(self)
+        self:accelerate(3.4288)
+        :diffusealpha(0.8)
+        :decelerate(3.4288)
+        :diffusealpha(0)
+        :queuecommand("Flash")
+      end
     },
 
-    LoadActor(THEME:GetPathG("","PressCenterStep"))..{
-        InitCommand=function(self)
-            self:xy(SCREEN_LEFT+150,SCREEN_BOTTOM-100)
-            :zoom(0.75,0.75)
-        end
-    },
+	},
 
-    LoadActor(THEME:GetPathG("","PressCenterStep"))..{
-        InitCommand=function(self)
-            self:xy(SCREEN_RIGHT-150,SCREEN_BOTTOM-100)
-            :zoom(0.75,0.75)
-        end
-    },
+  LoadActor(THEME:GetPathG("","PressCenterStep"))..{
+  	InitCommand=function(self)
+      self:xy(SCREEN_LEFT+150,SCREEN_BOTTOM-100)
+      :zoom(0.75,0.75)
+    end
+  },
 
-    ----------------
-    -- Info Stuff --
-    ----------------
+  LoadActor(THEME:GetPathG("","PressCenterStep"))..{
+    InitCommand=function(self)
+      self:xy(SCREEN_RIGHT-150,SCREEN_BOTTOM-100)
+      :zoom(0.75,0.75)
+    end
+  },
 
-    LoadFont("Montserrat semibold 20px")..{
-        InitCommand=function(self)
-            self:settext(SongStats.."\n"..sm_version)
-            :zoom(0.5)
-            :xy(SCREEN_LEFT+8, SCREEN_TOP+8)
-            :vertalign(top)
-            :horizalign(left)
-            :shadowlength(1)
-        end
-    }
+  ----------------
+  -- Info Stuff --
+  ----------------
+
+  LoadFont("Montserrat semibold 20px")..{
+    InitCommand=function(self)
+      self:settext(SongStats.."\n"..sm_version)
+      :zoom(0.5)
+      :xy(SCREEN_LEFT+8, SCREEN_TOP+8)
+      :vertalign(top)
+      :horizalign(left)
+      :shadowlength(1)
+    end
+  }
 
 }
 
