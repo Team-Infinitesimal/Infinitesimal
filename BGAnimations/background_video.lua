efddc95376511e868f3e6a0da3fb5aad029e37aa
SheepyChris <12992355+SheepyChris@users.noreply.github.com>
2021-03-13 10:36:48 -0500

    Improve memory usage with background video

--- BGAnimations/background_video.lua
+++ BGAnimations/background_video.lua
@@ -1,11 +1,7 @@
-local t = Def.ActorFrame {
-   Def.Sprite {
-		Texture=THEME:GetPathG("","bg"),
-        InitCommand=function(self)
-            self:Center()
-            :scaletocover(0, 0, SCREEN_RIGHT, SCREEN_BOTTOM)
-        end
-    }
+return Def.Sprite {
+	Texture=THEME:GetPathG("","bg"),
+	InitCommand=function(self)
+		self:Center()
+		:scaletocover(0, 0, SCREEN_RIGHT, SCREEN_BOTTOM)
+	end
 }
-
-return t
