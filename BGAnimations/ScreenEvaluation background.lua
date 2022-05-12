e21d2569846b3392b95e9c485a7e27d5c9d7c083
dj505 <jaydenb505@gmail.com>
2022-05-12 15:08:42 -0600

    Small aesthetic change to eval screen

--- BGAnimations/ScreenEvaluation background.lua
+++ BGAnimations/ScreenEvaluation background.lua
@@ -13,5 +13,25 @@ return Def.ActorFrame {
 				self:Load(Path):scale_or_crop_background():diffusealpha(0.5)
             end
         end
+    },
+
+    -- Grid
+    LoadActor(THEME:GetPathG("", "Grid"))..{
+        InitCommand=function(self)
+            self:diffusealpha(0)
+            :linear(1)
+            :diffusealpha(0.5)
+        end
+    },
+
+    Def.Sprite {
+      Name="Scanlines",
+      Texture=THEME:GetPathG("", "Scanline"),
+        InitCommand=function(self)
+            self:customtexturerect(0,0,SCREEN_WIDTH*4/16,SCREEN_HEIGHT*4/16)
+            :zoomto(SCREEN_WIDTH, SCREEN_HEIGHT):Center()
+            :diffusealpha(0.25)
+        end
     }
+
 }
