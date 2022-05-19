f79d99dcd7e48a2ec565fe03a2275ff45ce37f23
dj505 <jaydenb505@gmail.com>
2022-05-18 21:56:53 -0600

    whoops forgot a thing

--- BGAnimations/AnimatedBackground.lua
+++ BGAnimations/AnimatedBackground.lua
@@ -1,10 +1,18 @@
 if IsVideoBackground then
-    return Def.Sprite {
-        Texture=THEME:GetPathG("", "_Background.mp4"),
-        InitCommand=function(self)
-            self:scaletocover(0, 0, SCREEN_RIGHT, SCREEN_BOTTOM):Center()
-        end
+    local t = Def.ActorFrame {
+        Def.Sprite {
+            Texture=THEME:GetPathG("", "_Background.mp4"),
+            InitCommand=function(self)
+                self:scaletocover(0, 0, SCREEN_RIGHT, SCREEN_BOTTOM):Center()
+            end
+        },
+
+        -- Stop looking at the code this is supposed to be a surprise >:(
+        LoadActor(THEME:GetPathG("", "Background/confetti"))..{
+            Condition=IsAnniversary()
+        }
     }
+    return t
 else
     return LoadActor(THEME:GetPathG("", "Background"))
 end
