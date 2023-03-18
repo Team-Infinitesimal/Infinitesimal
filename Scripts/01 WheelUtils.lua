44d0088b265aa7b39d8ca6abe163ea39889e8312
Chris <12992355+SheepyChris@users.noreply.github.com>
2023-03-17 21:19:40 -0400

    Revert everything else lol

--- Scripts/01 WheelUtils.lua
+++ Scripts/01 WheelUtils.lua
@@ -1,13 +1,9 @@
-function WheelTransform(self, OffsetFromCenter, ItemIndex, NumItems) 
-    local absOffset = math.abs(OffsetFromCenter)
-    local Spacing = absOffset / math.pi
-    local rotation = clamp(OffsetFromCenter * 36, -85, 85)
-    local zoom = clamp(1.1 - (absOffset / 3), 0.8, 1.1)
-
+function WheelTransform (self, OffsetFromCenter, ItemIndex, NumItems) 
+    local Spacing = math.abs(math.sin(OffsetFromCenter / math.pi))
     self:x(OffsetFromCenter * (250 - Spacing * 100))
-    self:rotationy(rotation)
-    self:z(-absOffset)
-    self:zoom(zoom)
+    self:rotationy(clamp(OffsetFromCenter * 36, -85, 85))
+    self:z(-math.abs(OffsetFromCenter))
+    self:zoom(clamp(1.1 - (math.abs(OffsetFromCenter) / 3), 0.8, 1.1))
 end
 
 
@@ -18,22 +14,3 @@ function WheelTransformGroup (self, OffsetFromCenter, ItemIndex, NumItems)
     
     self.container:zoom(clamp(1.1 - (math.abs(OffsetFromCenter) / 3), 0.8, 1.1))
 end
-
-
--------------------------------------------------------------------------------------------------------------------
--- Better version of function, but commented to provide a reference instead of getting in the way of development --
--------------------------------------------------------------------------------------------------------------------
--- local WHEEL_X_OFFSET = 250
--- local WHEEL_X_SPACING = 100
--- local WHEEL_ROTATION_LIMIT = 85
--- local WHEEL_ZOOM_MIN = 0.8
--- local WHEEL_ZOOM_MAX = 1.1
-
--- function WheelTransformGroup (self, OffsetFromCenter, ItemIndex, NumItems) 
---     local container = self.container
---     local spacing = math.abs(math.sin(OffsetFromCenter / math.pi))
---     container:x(OffsetFromCenter * (WHEEL_X_OFFSET - spacing * WHEEL_X_SPACING))
---     container:rotationy(clamp(OffsetFromCenter * 30, -WHEEL_ROTATION_LIMIT, WHEEL_ROTATION_LIMIT))
-    
---     container:zoom(clamp(WHEEL_ZOOM_MAX - (math.abs(OffsetFromCenter) / 3), WHEEL_ZOOM_MIN, WHEEL_ZOOM_MAX))
--- end
