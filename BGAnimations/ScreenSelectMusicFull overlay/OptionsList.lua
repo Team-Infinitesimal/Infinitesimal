f03515f792dfe023571afd49655b450f0f6939ad
dj505 <dj505@users.noreply.github.com>
2023-08-17 18:22:19 -0600

    Def.OptionsList position fixes

--- BGAnimations/ScreenSelectMusicFull overlay/OptionsList.lua
+++ BGAnimations/ScreenSelectMusicFull overlay/OptionsList.lua
@@ -1,22 +1,24 @@
 local t = Def.ActorFrame {
     Def.OptionsList {
         Player=PLAYER_1,
+        InitCommand=function(self) self:xy(200, SCREEN_CENTER_Y-180):draworder(100) end,
         CodeMessageCommand=function(self, params)
             if params.Name == "OpenOpList" and params.PlayerNumber == PLAYER_1 then
                 self:Open()
             end
         end
     },
-    
+
     Def.OptionsList {
         Player=PLAYER_2,
+        InitCommand=function(self) self:xy(SCREEN_RIGHT-200, SCREEN_CENTER_Y-180):draworder(100) end,
         CodeMessageCommand=function(self, params)
             if params.Name == "OpenOpList" and params.PlayerNumber == PLAYER_2 then
                 self:Open()
             end
         end
     },
-    
+
     Def.Sound {
         File=THEME:GetPathS("", "OpenCommandWindow"),
         CodeMessageCommand=function(self, params)
@@ -52,43 +54,43 @@ local t = Def.ActorFrame {
 for pn in ivalues(GAMESTATE:GetHumanPlayers()) do
     -- This will allow us to determine the position of the list
     local OptionsListActor, OptionsListMenu
-    
+
     t[#t+1] = Def.ActorFrame {
         InitCommand=function(self,params)
             self:xy(pn == PLAYER_1 and -200 or SCREEN_RIGHT + 200, SCREEN_CENTER_Y - 70)
         end,
-        
+
         OnCommand=function(self)
             OptionsListActor = SCREENMAN:GetTopScreen():GetChild("OptionsList" .. pname(pn))
         end,
-        
+
         -- Move the list around when opened/closed
         OptionsListOpenedMessageCommand=function(self, params)
             if params.Player == pn then
                 self:stoptweening():easeoutexpo(0.5):x(pn == PLAYER_1 and 200 or SCREEN_RIGHT - 200)
             end
         end,
-        
+
         OptionsListClosedMessageCommand=function(self, params)
-            if params.Player == pn then 
+            if params.Player == pn then
                 self:stoptweening():easeoutexpo(0.5):x(pn == PLAYER_1 and -200 or SCREEN_RIGHT + 200)
             end
         end,
-        
+
         -- Make us able to view what menu we're in later (and also adjust its position)
         OptionsMenuChangedMessageCommand=function(self, params)
-            if params.Player == pn then 
+            if params.Player == pn then
                 OptionsListMenu = params.Menu
-                
+
                 self:playcommand("Adjust", params)
             end
         end,
-        
+
         OptionsListLeftMessageCommand=function(self, params) self:playcommand("Adjust", params) end,
         OptionsListRightMessageCommand=function(self, params) self:playcommand("Adjust", params) end,
         OptionsListStartMessageCommand=function(self, params) self:playcommand("Adjust", params) end,
         OptionsListQuickChangeMessageCommand=function(self, params) self:playcommand("Adjust", params) end,
-        
+
         -- To avoid overflowing the list, we will hide the outer parts and
         -- dynamically move the entire list's vertical position relative
         -- to what the player is currently selecting
@@ -104,17 +106,17 @@ for pn in ivalues(GAMESTATE:GetHumanPlayers()) do
                 end
             end
         end,
-        
+
         Def.Sprite {
             Texture=THEME:GetPathG("", "MusicWheel/Options"),
             InitCommand=function(self) self:zoom(0.7) end
         },
-        
+
         -- Masks that will hide the off limits portion of the list, shhh
         Def.Quad {
             InitCommand=function(self) self:setsize(206, 170):xy(0, -205):MaskSource() end,
         },
-        
+
         Def.Quad {
             InitCommand=function(self) self:setsize(206, 268):xy(0, 296):MaskSource() end,
         }
